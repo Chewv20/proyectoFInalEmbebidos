c41db98a423fcc413f50eb2b123b6b5d5ec56b2e
Chewv20 <72229009+Chewv20@users.noreply.github.com>
2022-05-30 23:07:25 -0500

    Update copy_to_autostart.sh
    license

--- src/copy_to_autostart.sh
+++ src/copy_to_autostart.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+# ## ###############################################
+#
+# disableInputs.py
+#
+#
+# Autor: César Augusto Martínez Franco
+#        Lisset Noriega Domínguez
+#        Quiroz hernandez Rodolfo
+#        Jesús Arturo Vázquez Zaragoza
+# License: MIT
+#
+# ## ###############################################
 sudo cp -r ./autostart ~/.config/
 sudo cp Rasp.lyt ~/.qjoypad3/
 sudo cp mednaffe.conf ~/.config/
